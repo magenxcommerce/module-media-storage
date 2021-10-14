d9f94fb86f1b49fc9d6c925787d8a2f281d85acb
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 18:47:12 -0700

    Magento Fork initial commit

--- Model/File/Storage.php
+++ Model/File/Storage.php
@@ -11,7 +11,7 @@ use Magento\Framework\Filesystem;
 use Magento\Framework\Model\AbstractModel;
 
 /**
- * Media File Storage
+ * Class Storage
  *
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  * @api
@@ -156,8 +156,8 @@ class Storage extends AbstractModel
 
     /**
      * Retrieve storage model
-     *
      * If storage not defined - retrieve current storage
+     *
      * params = array(
      *  connection  => string,  - define connection for model if needed
      *  init        => bool     - force initialization process for storage model
@@ -289,10 +289,9 @@ class Storage extends AbstractModel
         $config['media_directory'] = $this->filesystem->getDirectoryRead(DirectoryList::MEDIA)->getAbsolutePath();
 
         $allowedResources = $this->_coreConfig->getValue(self::XML_PATH_MEDIA_RESOURCE_WHITELIST, 'default');
-        array_walk_recursive($allowedResources, function ($value) use (&$resources) {
-            $resources[] = $value;
-        }, $resources);
-        $config['allowed_resources'] = $resources;
+        foreach ($allowedResources as $allowedResource) {
+            $config['allowed_resources'][] = $allowedResource;
+        }
 
         $config['update_time'] = $this->_scopeConfig->getValue(
             self::XML_PATH_MEDIA_UPDATE_TIME,
